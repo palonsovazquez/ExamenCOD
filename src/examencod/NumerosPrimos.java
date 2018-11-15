fca2b857bfa0050294dee446e2f329ce4d8e696a
palonsovazquez <pav.vigo@gmail.com>
2018-11-15 09:55:40 +0100

    Refactorizacion de NumerosPrimos 2.cambio de nombres

--- src/examencod/NumerosPrimos.java
+++ src/examencod/NumerosPrimos.java
@@ -13,13 +13,13 @@ public class NumerosPrimos { // cambio el nombre del fichero para que correspond
     //LIstar los numeros según el numero de digitos indicado
     //Considero solo hasta numeros menores a 100000 (5 digitos), 
     //por el hecho de k buscar numeros primos a partir de 6 digitos, el proceso se hace muy lento.
-    public static boolean p = false;
+    public static boolean booleanPrimo = false;
 
     public static void main(String arg[]) {
-        int numDigitos = 0;
-        int ndigitos = 0;
-        numDigitos = Integer.parseInt(arg[0]);
-        if (numDigitos <= 0) {
+        int intNumDigitos = 0;
+        int intNDigitos = 0;
+        intNumDigitos = Integer.parseInt(arg[0]);
+        if (intNumDigitos <= 0) {
             System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
         }
         for (int i = 1; i <= 99999; i++) {
@@ -31,14 +31,14 @@ public class NumerosPrimos { // cambio el nombre del fichero para que correspond
                 divisionEntera = divisionEntera / 10;
                 contador++;
             }
-            ndigitos = contador;
+            intNDigitos = contador;
 
-            if (ndigitos == numDigitos) {
+            if (intNDigitos == intNumDigitos) {
                 if (i < 4) {
-                    p = true;
+                    booleanPrimo = true;
                 } else {
                     if (i % 2 == 0) {
-                        p = false;
+                        booleanPrimo = false;
                     } else {
                         int contador1 = 0;
                         int i1 = 1;
@@ -58,12 +58,12 @@ public class NumerosPrimos { // cambio el nombre del fichero para que correspond
                         }
 
                         if (contador1 == 1) {
-                            p = true;
+                            booleanPrimo = true;
                         }
                     }
                 }
 
-                if (p == true) {
+                if (booleanPrimo == true) {
                     System.out.println(i);
                 }
             }
